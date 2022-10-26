a37b7ad8c7f283646b4b993463d67a227a5c0566
sleepy068 <58592380+sleepy068@users.noreply.github.com>
2022-10-26 19:05:27 +1100

    sync

--- Resources/Code/Sleepys_MorePsycasts/Class1.cs
+++ Resources/Code/Sleepys_MorePsycasts/Class1.cs
@@ -3,10 +3,85 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Verse;
+using RimWorld;
 
 namespace Sleepys_MorePsycasts
 {
-    public class Class1
+    public class CompProperties_SLP_AbilityGiveTwoHediff :
+       CompProperties_AbilityGiveHediff
     {
+        public HediffDef hediffDef2;
     }
+
+    public class CompAbilityEffect_GiveTwoHediff : CompAbilityEffect_WithDuration
+    {
+        public CompProperties_SLP_AbilityGiveTwoHediff Props => (CompProperties_SLP_AbilityGiveTwoHediff)this.props;
+
+        public override void Apply(LocalTargetInfo target, LocalTargetInfo dest)
+        {
+            base.Apply(target, dest);
+            if (!this.Props.onlyApplyToSelf && this.Props.applyToTarget)
+                this.ApplyInner(target.Pawn, this.parent.pawn);
+            if (!this.Props.applyToSelf && !this.Props.onlyApplyToSelf)
+                return;
+            this.ApplyInner(this.parent.pawn, target.Pawn);
+        }
+
+        protected void ApplyInner(Pawn target, Pawn other)
+        {
+            if (target == null)
+                return;
+
+            if (this.Props.replaceExisting)
+            {
+                Hediff firstHediffOfDef = target.health.hediffSet.GetFirstHediffOfDef(this.Props.hediffDef);
+                if (firstHediffOfDef != null)
+                    target.health.RemoveHediff(firstHediffOfDef);
+            }
+            if (this.Props.replaceExisting)
+            {
+                Hediff firstHediffOfDef2 = target.health.hediffSet.GetFirstHediffOfDef(this.Props.hediffDef2);
+                if (firstHediffOfDef2 != null)
+                    target.health.RemoveHediff(firstHediffOfDef2);
+            }
+
+            Hediff hediff = HediffMaker.MakeHediff(this.Props.hediffDef, target, this.Props.onlyBrain ? target.health.hediffSet.GetBrain() : (BodyPartRecord)null);
+            HediffComp_Disappears comp1 = hediff.TryGetComp<HediffComp_Disappears>();
+            if (comp1 != null)
+                comp1.ticksToDisappear = this.GetDurationSeconds(target).SecondsToTicks();
+            if ((double)this.Props.severity >= 0.0)
+                hediff.Severity = this.Props.severity;
+            HediffComp_Link comp2 = hediff.TryGetComp<HediffComp_Link>();
+            if (comp2 != null)
+            {
+                comp2.other = (Thing)other;
+                comp2.drawConnection = target == this.parent.pawn;
+            }
+            target.health.AddHediff(hediff);
+
+            Hediff hediff2 = HediffMaker.MakeHediff(this.Props.hediffDef2, target, this.Props.onlyBrain ? target.health.hediffSet.GetBrain() : (BodyPartRecord)null);
+            HediffComp_Disappears comp1a = hediff2.TryGetComp<HediffComp_Disappears>();
+            if (comp1a != null)
+                comp1a.ticksToDisappear = this.GetDurationSeconds(target).SecondsToTicks();
+            if ((double)this.Props.severity >= 0.0)
+                hediff2.Severity = this.Props.severity;
+            HediffComp_Link comp2a = hediff2.TryGetComp<HediffComp_Link>();
+            if (comp2a != null)
+            {
+                comp2a.other = (Thing)other;
+                comp2a.drawConnection = target == this.parent.pawn;
+            }
+            target.health.AddHediff(hediff2);
+        }
+    }
+
+    [DefOf]
+    public class HediffDefOf
+    {
+        public static HediffDef SLP_Psycast_FlashHeal;
+        public static HediffDef SLP_HealingPains;
+
+    }
+
 }
