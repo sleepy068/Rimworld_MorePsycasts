1a2d04986563314363c702b3845fee109b06e11a
sleepy068 <58592380+sleepy068@users.noreply.github.com>
2025-04-08 15:58:00 +1000

    sync

---
+++ Source/Sleepys More Psycasts Vol2/1.5/Main.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Verse;
+using Verse.Sound;
+using Verse.AI.Group;
+using RimWorld;
+using RimWorld.Planet;
+using UnityEngine;
+
+
+namespace Sleepys_MorePsycastsVol2
+{
+    //Comp Properties
+
+
+    //Comp Ability Effect
+
+
+    //Hediff
+
+    
+    //Utility Code
+    
+
+}
