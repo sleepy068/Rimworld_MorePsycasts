d33286a0bf29f982695ef9386f54c9adba50bbb0
sleepy068 <58592380+sleepy068@users.noreply.github.com>
2025-04-22 21:48:56 +1000

    sync

--- Source/Sleepys More Psycasts Vol2/1.5/Main.cs
+++ Source/Sleepys More Psycasts Vol2/1.5/Main.cs
@@ -12,15 +12,110 @@ using UnityEngine;
 namespace Sleepys_MorePsycastsVol2
 {
     //Comp Properties
+    public class CompProperties_SLP_AbilityGaspop : CompProperties_AbilityEffect
+    {
+        public float smokeRadius;
 
+        public CompProperties_SLP_AbilityGaspop() => this.compClass = typeof(CompAbilityEffect_SLP_Gaspop);
+    }
+
+    public class CompProperties_SLP_AbilityFoamskip : CompProperties_AbilityEffect
+    {
+        public CompProperties_SLP_AbilityFoamskip()
+        {
+            this.compClass = typeof(CompAbilityEffect_SLP_Foamskip);
+        }
+    }
 
     //Comp Ability Effect
+    public class CompAbilityEffect_SLP_Gaspop : CompAbilityEffect
+    {
+        public CompProperties_SLP_AbilityGaspop Props => (CompProperties_SLP_AbilityGaspop)this.props;
 
 
-    //Hediff
+        public override void Apply(LocalTargetInfo target, LocalTargetInfo dest)
+        {
+            base.Apply(target, dest);
+            GenExplosion.DoExplosion(target.Cell, this.parent.pawn.MapHeld, this.Props.smokeRadius, DamageDefOf.ToxGas, (Thing)null, postExplosionGasType: new GasType?(GasType.ToxGas));
+        }
 
-    
-    //Utility Code
-    
+        public override void DrawEffectPreview(LocalTargetInfo target)
+        {
+            GenDraw.DrawRadiusRing(target.Cell, this.Props.smokeRadius);
+        }
+
+    }
+
+	public class CompAbilityEffect_SLP_Foamskip : CompAbilityEffect
+	{
+		public override void Apply(LocalTargetInfo target, LocalTargetInfo dest)
+		{
+			base.Apply(target, dest);
+			Map map = parent.pawn.Map;
+			foreach (IntVec3 item in AffectedCells(target, map))
+			{
+				if (!item.InBounds(map))
+				{
+					continue;
+				}
+				List<Thing> thingList = item.GetThingList(map);
+				for (int num = thingList.Count - 1; num >= 0; num--)
+				{
+					if (thingList[num] is Fire)
+					{
+						thingList[num].Destroy();
+					}
+					else if (thingList[num] is Pawn pawn)
+					{
+						pawn.GetInvisibilityComp()?.DisruptInvisibility();
+					}
+				}
+				if (!item.Filled(map))
+				{
+					FilthMaker.TryMakeFilth(item, map, ThingDefOf.Filth_FireFoam);
+				}
+				FleckCreationData dataStatic = FleckMaker.GetDataStatic(item.ToVector3Shifted(), map, FleckDefOf.Smoke);
+				dataStatic.rotationRate = Rand.Range(-30, 30);
+				dataStatic.rotation = 90 * Rand.RangeInclusive(0, 3);
+				map.flecks.CreateFleck(dataStatic);
+				CompAbilityEffect_Teleport.SendSkipUsedSignal(item, parent.pawn);
+			}
+		}
+
+		private IEnumerable<IntVec3> AffectedCells(LocalTargetInfo target, Map map)
+		{
+			if (!target.Cell.InBounds(map) || target.Cell.Filled(parent.pawn.Map))
+			{
+				yield break;
+			}
+			foreach (IntVec3 item in GenRadial.RadialCellsAround(target.Cell, parent.def.EffectRadius, useCenter: true))
+			{
+				if (item.InBounds(map) && GenSight.LineOfSightToEdges(target.Cell, item, map, skipFirstCell: true))
+				{
+					yield return item;
+				}
+			}
+		}
+
+		public override void DrawEffectPreview(LocalTargetInfo target)
+		{
+			GenDraw.DrawFieldEdges(AffectedCells(target, parent.pawn.Map).ToList(), Valid(target) ? Color.white : Color.red);
+		}
+
+		public override bool Valid(LocalTargetInfo target, bool throwMessages = false)
+		{
+			if (target.Cell.Filled(parent.pawn.Map))
+			{
+				if (throwMessages)
+				{
+					Messages.Message("AbilityOccupiedCells".Translate(parent.def.LabelCap), target.ToTargetInfo(parent.pawn.Map), MessageTypeDefOf.RejectInput, historical: false);
+				}
+				return false;
+			}
+			return true;
+		}
+	}
+	
+	//Hediff
 
 }
--- Source/Sleepys More Psycasts Vol2/1.5/Properties/AssemblyInfo.cs
+++ Source/Sleepys More Psycasts Vol2/1.5/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Sleepys_MorePsycasts")]
+[assembly: AssemblyTitle("Sleepys_MorePsycastsVol2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Sleepys_MorePsycasts")]
+[assembly: AssemblyProduct("Sleepys_MorePsycastsVol2")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
